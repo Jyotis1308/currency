4a97d3974160f8c3010faaf4d3fe14c28775c708
Jyotis1308 <2022021228@mmmut.ac.in>
2025-05-15 17:38:51 +0530

    Add files via upload

---
+++ api-key.js
@@ -0,0 +1,2 @@
+//Paste your generated api Key here
+let apiKey = "1d70ce78e4355f628624036f";
---
+++ currency-codes.js
@@ -0,0 +1,8 @@
+currencies=[
+     "AED", "ARS", "AUD", "BDT", "BRL", "CAD", "CHF", "CLP", "CNY", "COP",
+  "CZK", "DKK", "EGP", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR",
+  "JPY", "KRW", "KWD", "LKR", "MXN", "MYR", "NGN", "NOK", "NPR", "NZD",
+  "PHP", "PKR", "PLN", "QAR", "RON", "RUB", "SAR", "SEK", "SGD", "THB",
+  "TRY", "USD", "VND", "ZAR"
+
+]
